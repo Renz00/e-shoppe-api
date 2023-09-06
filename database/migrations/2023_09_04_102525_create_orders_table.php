20945048fef7c000e86a76baaf84838064817749
Renz De la Torre <renzdelatorre00@gmail.com>
2023-09-06 18:51:48 +0800

    update 9-6-23

--- database/migrations/2023_09_04_102525_create_orders_table.php
+++ database/migrations/2023_09_04_102525_create_orders_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
             $table->integer('item_count');
             $table->foreignId('order_status');
             $table->integer('order_total');
