4828a4770308a224e9914a23b192d7599a1f06a5
Renz De la Torre <renzdelatorre00@gmail.com>
2023-10-02 17:03:28 +0800

    update 10-2-23

---
+++ database/migrations/2023_10_02_061134_create_vouchers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('voucher_name');
+            $table->integer('voucher_price_discount')->default(0);
+            $table->integer('voucher_shipping_discount')->default(0);
+            $table->double('voucher_minimum_price', 11, 2)->default(0);
+            $table->string('voucher_category')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
